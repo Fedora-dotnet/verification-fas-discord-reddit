db94fce257bc8907d5f41d49edbc650ff7467a8e
houndthe <kajm101@gmail.com>
2019-10-07 11:51:20 +0200

    Refactoring var

--- VerificationWeb/Services/RoleService.cs
+++ VerificationWeb/Services/RoleService.cs
@@ -21,23 +21,14 @@ namespace VerificationWeb.Services
 
         public async Task AssignRoleAsync(ulong userId, IEnumerable<string> roles)
         {
-            // TODO find the nullref bug
-            try
-            {
-                var rolesToAdd = new List<IRole>();
-                foreach (var x in roles)
+            var rolesToAdd = new List<IRole>();
+                foreach (string roleName in roles)
                 {
-                    var roleId = Config.DiscordRoles[x];
+                    var roleId = Config.DiscordRoles[roleName];
                     rolesToAdd.Add(_guild.GetRole(roleId));
                 }
 
-                await _guild.GetUser(userId).AddRolesAsync(rolesToAdd);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            await _guild.GetUser(userId).AddRolesAsync(rolesToAdd);
         }
     }
 }
