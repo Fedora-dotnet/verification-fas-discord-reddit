c1dd331ec133738154191d394f10242cfb6a81a0
Eric Gustavsson <egustavs@redhat.com>
2020-02-05 15:15:10 +0100

    fix: Use forwarded headers
    Makes things work with CloudFlare

--- VerificationWeb/Startup.cs
+++ VerificationWeb/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Logging;
@@ -81,6 +82,9 @@ namespace VerificationWeb
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
             app.UseAuthentication();
             app.UseSession();
             app.Use((context, next) =>
