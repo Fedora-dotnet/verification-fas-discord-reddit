50c311f4c637bf4ef1c01234d8d8ca91feddb9c2
houndthe <kajm101@gmail.com>
2020-05-21 18:52:56 +0200

    Upgraded Asp to 3.1

--- VerificationWeb/Startup.cs
+++ VerificationWeb/Startup.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Security.Claims;
-using System.Threading;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using RedditSharp;
 using VerificationWeb.Configuration;
 using VerificationWeb.Services;
@@ -67,11 +62,11 @@ namespace VerificationWeb
             services.AddSingleton(Config);
             services.AddSingleton<RoleService>();
             services.AddDiscordBot();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -85,7 +80,10 @@ namespace VerificationWeb
             app.UseForwardedHeaders(new ForwardedHeadersOptions {
                 ForwardedHeaders = ForwardedHeaders.All
             });
+            app.UseStaticFiles();
+            app.UseRouting();
             app.UseAuthentication();
+            app.UseCors();
             app.UseSession();
             app.Use((context, next) =>
             {
@@ -94,9 +92,10 @@ namespace VerificationWeb
                 return next();
             });
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseCookiePolicy();
-            app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
